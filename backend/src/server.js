757dffebed43ab42250034a43d90a6634c0f33b3
Kim Seok Yoon <kjkksu2@naver.com>
2022-02-05 21:04:59 +0900

    backend setup

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+const PORT = 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.post("/name", (req, res) => {
+  const { name } = req.body;
+  console.log(name);
+  return res.json("It works!");
+});
+
+app.listen(PORT, () => console.log(`✅ Listening on port ${PORT}`));
